514031bec0a1acd5e51621906fa38c1c6fd113e0
arpitk02 <akshkumar852@gmail.com>
2020-05-31 08:43:14 +0530

    planning

--- js/home.js
+++ js/home.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     })
 })
 
+
 $(document).ready(function(){
     //fadeinSections();
     // $('.portfoliobox').waypoint(function(direction){
@@ -132,3 +133,69 @@ $(document).ready(function(){
         }
     }
 });
+
+
+$(document).ready(function(){
+    fadeinSections();
+
+    function fadeinSections(verticalSpace) {
+      var pageTop = jQuery(document).scrollTop();
+      window.pageBottom = pageTop + jQuery(window).height();
+      var tags = jQuery(".fadein");
+
+      for (var i = 0; i < tags.length; i++) {
+          var tag = tags[i];
+        
+          /* If the object is visible in the window by the specified amount of pixels, fade it it */
+          if (jQuery(tag).offset().top < window.pageBottom - verticalSpace) {
+              jQuery(tag).addClass("visible");
+          } else {
+              jQuery(tag).removeClass("visible");
+          }
+      }
+
+
+        var tags = jQuery(".fadeintop");
+        for (var i = 0; i < tags.length; i++) {
+          var tag = tags[i];
+        
+          /* If the object is visible in the window by the specified amount of pixels, fade it it */
+          if (jQuery(tag).offset().top < window.pageBottom - verticalSpace) {
+              jQuery(tag).addClass("visible");
+          } else {
+              jQuery(tag).removeClass("visible");
+          }
+      }
+      var tags = jQuery(".line");
+        for (var i = 0; i < tags.length; i++) {
+          var tag = tags[i];
+        
+          /* If the object is visible in the window by the specified amount of pixels, fade it it */
+          if (jQuery(tag).offset().top < window.pageBottom - verticalSpace) {
+              jQuery(tag).addClass("visible");
+          } else {
+              
+          }
+      }
+      
+      
+       var tags = jQuery(".fadeinbottom");
+        for (var i = 0; i < tags.length; i++) {
+          var tag = tags[i];
+        
+          /* If the object is visible in the window by the specified amount of pixels, fade it it */
+          if (jQuery(tag).offset().top < window.pageBottom - verticalSpace) {
+              jQuery(tag).addClass("visible");
+          } else {
+              jQuery(tag).removeClass("visible");
+          }
+      }  
+    }
+
+    jQuery(document).ready(function(e) {
+        fadeinSections(50);
+    });
+    jQuery(document).on("scroll", function () {
+        fadeinSections(150);
+    });
+});    
