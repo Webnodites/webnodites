8ca30ae52586e598aa2aca23a11ed132bc86c1c3
arpitk02 <akshkumar852@gmail.com>
2020-06-07 11:12:59 +0530

    Background

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-VANTA.TOPOLOGY({
-    el: "#topology",
-    mouseControls: true,
-    touchControls: true,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x1309d9,
-    backgroundColor: 0x0
-  })
+VANTA.RINGS({
+  el: "#topology",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
